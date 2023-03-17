4a97e0f043d34ed6e267b36d513696f924b3992a
j0t4ku <j.ismar00@gmail.com>
2023-03-17 09:57:18 -0300

    Complete tema 11: buenas practicas

---
+++ src/pages/tasks/TasksPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskListComponent from '../../components/container/task_list';
+
+const TasksPage = () => {
+    return (
+        <div>
+            <h1>Tasks Page</h1>
+            <TaskListComponent></TaskListComponent>
+        </div>
+    );
+}
+
+export default TasksPage;
